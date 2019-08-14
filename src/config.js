32455b6a3566c666142b8e2da9e14e5dc787b6e9
Paul Han <kyutae.han93@gmail.com>
2019-08-14 11:36:55 -0400

    getting things ready for heroku push

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://tester@localhost/tftBuilds',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://tester@localhost/tftBuilds',
   TEST_DB_URL:process.env.DB_URL || 'postgresql://tester@localhost/tftBuilds',
 
   JWT_SECRET: process.env.JWT_SECRET || 'lkglhjsigflkgjdsudkgkhjjskfkhsjoeuwk',
